0ff6b8553038149a1aa964ce850aacca605aa188
Kelton Zhang <zhuoran2@andrew.cmu.edu>
2021-06-18 16:37:03 +0800

    fix: change config, setup, dependencies

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ import setuptools
 
 
 setuptools.setup(
-    name="executor-image-normalizer",
+    name="jinahub-clip",
     version="2.0",
     author='Jina Dev Team',
     author_email='dev-team@jina.ai',
-    description="Executor that normalizes images",
-    url="https://github.com/jina-ai/executor-image-normalizer",
+    description="Executor that encodes text with CLIP model",
+    url="https://github.com/jina-ai/executor-text-clip-text-encoder",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    py_modules=['jinahub.image.normalizer'],
-    package_dir={'jinahub.image': '.'},
+    py_modules=['jinahub.encoder.ClipTextEncoder'],
+    package_dir={'jinahub.text': '.'},
     install_requires=open("requirements.txt").readlines(),
     python_requires=">=3.7",
 )
